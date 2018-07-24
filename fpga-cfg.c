2bd1d36c62fa57c7b8d6b097ba8a172081449993
Anatolij Gustschin <agust@denx.de>
2018-07-24 19:47:11 +0200

    fpga-cfg: use updated xlnx fpga manager name in v4.15 kernel
    With Altera PS-SPI manager we create unique config interface directory
    for each registered manager and its name contains trailing string
    matching the PS-SPI slave name. Use similar pattern with Xilinx
    Spartan-6 FPGA interface.
    
    See v4.15 kernel commit-ID 0f41d35b97 (fpga manager: xilinx-spi: Use
    unique FPGA manager names).

--- fpga-cfg.c
+++ fpga-cfg.c
@@ -39,12 +39,13 @@
 
 #define FPGA_DRV_STRING		"fpga_cfg"
 #define FPP_RING_MGR_NAME	"ftdi-fpp-fpga-mgr"
-#define SPI_XLNX_MGR_NAME	"Xilinx Slave Serial FPGA Manager"
 
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 11, 0)
-#define SPI_RING_MGR_NAME      "Altera Cyclone PS SPI FPGA Manager"
+#define SPI_RING_MGR_NAME	"Altera Cyclone PS SPI FPGA Manager"
+#define SPI_XLNX_MGR_NAME	"Xilinx Slave Serial FPGA Manager"
 #else
 #define SPI_RING_MGR_NAME	"altera-ps-spi"
+#define SPI_XLNX_MGR_NAME	"xlnx-slave-spi"
 #endif
 
 #define FPGA_CFG_HISTORY_ENTRIES_MIN	500
